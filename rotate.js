d498e4561b233cadfb4244548faa3cf4169644f1
Julia Pontes <47482821+juliapcp@users.noreply.github.com>
2019-03-07 17:18:54 -0300

    Add files via upload

--- rotate.js
+++ rotate.js
@@ -1,10 +1,12 @@
-let rotate = function (txt, horario = true) { //por padrao será horario caso nao seja passado um valor pra algum parametro 
+let rotate2 = function (txt, horario = true) { //por padrao será horario caso nao seja passado um valor pra algum parametro 
+    //debugger; //para o codigo nesse ponto, aí ao utilizar o f9 no source ao lado do console no chrome, vemos passo a passo passando por cada linha
+    //indo na margem e clicando pra ficar roxo também irá parar o codigo naquela linha ( no sources ) 
     const len = txt.length - 1;
     if (horario === true) return txt[len] + txt.substr(0, len); //se for true entra
     else return txt.substr(1,len) + txt[0];
 }
-console.log(rotate('ifrs')); //sifr
-console.log(rotate('ifrs',false)); //frsi
+console.log(rotate2('ifrs')); //sifr
+console.log(rotate2('ifrs',false)); //frsi
 
 //o que é falso:
 //false
@@ -21,5 +23,5 @@ if (v) {
 //colocar if(variavel) avalia uma variavel como true or false
 const n = '';
 if (n) {
-    console.log('n é false');
+    console.log('n é false'); //nao imprime
 }
